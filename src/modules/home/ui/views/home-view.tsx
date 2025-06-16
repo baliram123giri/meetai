bbb5196f855f3335ed07137eab58dec6f51890a4
baliram123giri <giri71401@gmail.com>
2025-06-17 01:42:03 +0530

    08:trpc setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,41 +1,13 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import Image from "next/image";
-import React from "react";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function HomeView() {
-  const { data: session, isPending } = authClient.useSession();
-  if (isPending) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        Loading...
-      </div>
-    );
-  }
-  if (session) {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-        <Image
-          src={session.user.image || "/default-avatar.png"}
-          height={100}
-          width={100}
-          alt="profile picture"
-          className="rounded-full  shadow-xl"
-        />
-        <h1 className="text-2xl font-bold">Welcome, {session.user.name}!</h1>
-        <p className="text-lg">You are already logged in.</p>
-        <Button
-          variant={"default"}
-          onClick={() =>
-            authClient
-              .signOut()
-              .then(() => (window.location.pathname = "/sign-in"))
-          }
-        >
-          Sign Out
-        </Button>
-      </div>
-    );
-  }
+  const trpc = useTRPC();
+  const { data } = useQuery(
+    trpc.hello.queryOptions({ text: "Testing Baliram" })
+  );
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">{data?.greeting}</div>
+  );
 }
