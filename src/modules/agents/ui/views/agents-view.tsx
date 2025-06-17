4dc2dd35dac6bd4084e2621042417e2b8e0c2813
baliram123giri <giri71401@gmail.com>
2025-06-17 22:52:56 +0530

    10:responsive dialog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import ResponsiveDialog from "@/components/responsive-dialog";
+import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import React from "react";
@@ -8,5 +10,17 @@ export default function AgentsView() {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div>
+      <ResponsiveDialog
+        open={true}
+        onOpenChange={() => {}}
+        title="Agents"
+        description="List of agents"
+      >
+        <Button>Section</Button>
+      </ResponsiveDialog>
+      {JSON.stringify(data, null, 2)}
+    </div>
+  );
 }
